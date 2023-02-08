154304de0f6ba87424874b322be6b8811ca33d8b
Juan Angel <46574873+kiikedls@users.noreply.github.com>
2023-02-07 18:39:16 -0600

    agregue las factories, controladores y migraciones de usuarios, cateogrias e items a la migracion tambien hay seeders que agregan usuarios de prueba

---
+++ database/migrations/2023_02_07_235542_create_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->string('name');
+            $table->string('description');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        Schema::dropIfExists('items');
+    }
+};
