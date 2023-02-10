823057146f673ff4c08ccaf1b96fc4676ba27d9b
Juan Angel <46574873+kiikedls@users.noreply.github.com>
2023-02-10 17:20:39 -0600

    construccion de los modelos

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function scopeOfCategory($query, $categoryId)
+    {
+        return $query->where("category_id", $categoryId);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -30,4 +30,39 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     protected $hidden = [
         'password',
     ];
+
+    public function categories(){
+        return $this->hasMany(Category::class);
+    }
+
+    public function items(){
+        return $this->hasMany(Item::class);
+    }
+
+    public function addCategory(Category $category){
+        return $this->categories()->save($category);
+    }
+
+    public function deleteCategory($categoryId){
+        $this->categories()->find($categoryId)->delete();
+        return ["message"=>"la lista de compras fue borrada"];
+    }
+
+    public function hasDuplicateCategory($categoryName){
+        return $this->categories()->where("name",$categoryName)->count();
+    }
+
+    public function addItem(Item $item,$categoryId){
+        return $this->categories()->find($categoryId)
+        ->items()->create([
+            "name"=>$item->name,
+            "description"=>$item->description,
+            "user_id"=>$this->id
+        ]);
+    }
+
+    public function hasDuplicateItem($categoryId,$itemName){
+        return $this->categories()->find($categoryId)->items()->where("name",$itemName)->count();
+    }
+
 }
