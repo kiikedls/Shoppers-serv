8710d34877b54246207f4ea00196d936c6af04c3
Juan Angel <46574873+kiikedls@users.noreply.github.com>
2023-02-10 17:21:13 -0600

    primer construccion de los controladores

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(){
+        return Category::all();
+    }
+
+    public function store(Request $req){
+        try{
+            $category=new Category();
+            $category->user_id=$req->usr;
+            $category->name=$req->name;
+            //$category->
+
+            if($category->save()){
+                return response()->json(['status'=>'success','message'=>'categoria creada exitosamente']);
+            }
+        }catch(\Exception $e){
+            return response()->json(['status'=>'error','message'=>$e->getMessage()]);
+        }
+    }
 }
